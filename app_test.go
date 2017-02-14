79cecf295d2bfa30a3adcf6be9f8b278225f0a16
gujarats <gujarat.santana@gmail.com>
2017-02-14 21:59:58 +0700

    add response test

--- app_test.go
+++ app_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -10,27 +12,48 @@ import (
 
 	"github.com/training_project/controller/review"
 	"github.com/training_project/controller/review/struct"
+	reviewModel "github.com/training_project/model/review"
 	"github.com/training_project/model/review/interface"
 )
 
-func TestEndPoint(t *testing.T) {
+func TestEndPointSatusOk(t *testing.T) {
 	data := url.Values{}
-	data.Set("shop_id", "171")
+	data.Set("shop_id", "17112321") // dummy shopID value. inside database is not exist
 
+	//we pass a dummy value to pass the required params
 	req := httptest.NewRequest("POST", "/", bytes.NewBufferString(data.Encode()))
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
 
 	w := httptest.NewRecorder()
 
-	//  create mock object to test handler
-	reviewMock := reviewMethod.ReviewDataMock{ShopID: 12}
+	//  create mock object to test handler. Must called first before handler
+	reviewMock := reviewMethod.ReviewDataMock{}
 	reviewStruct.SetStruct(reviewMock)
 
 	// we're going to test handler
 	review.CheckDataExist(w, req)
 
+	// check the status code
 	if w.Code != http.StatusOK {
 		t.Errorf("Error Code !! actual : %d, expected : %d\n", w.Code, http.StatusOK)
 	}
+
+	// check the response
+	resp := w.Body.Bytes()
+	if resp == nil {
+		t.Error("Error Body result Empty")
+	}
+
+	reviewResp := reviewModel.ReviewResponse{}
+	err := json.Unmarshal(resp, &reviewResp)
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Printf("result response = %+v\n", reviewResp)
+	if reviewResp.Message != "Data Exist" {
+		t.Errorf("Error response:: actual = %v, expected = %v\n", reviewMock.Message, "Data Exist")
+	}
+
 }
