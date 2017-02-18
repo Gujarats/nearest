887001f7109c245130e9e440996a9ef2de9fb3d5
gujarats <gujarat.santana@gmail.com>
2017-02-18 13:33:58 +0700

    change global

--- model/global/global.go
+++ model/global/global.go
@@ -1,7 +1,19 @@
 package global
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type Response struct {
 	Status  string
 	Message string
 	Data    interface{}
+}
+
+func SetResponse(w http.ResponseWriter, Status string, Message string) {
+	resp := Response{}
+	resp.Status = Status
+	resp.Message = Message
+	json.NewEncoder(w).Encode(resp)
 }
