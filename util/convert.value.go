5fdd174e6fdbd5c605d942734099c80389bde5e3
gujarats <gujarat.santana@gmail.com>
2017-02-28 14:13:15 +0700

    convert string to int64

--- util/convert.value.go
+++ util/convert.value.go
@@ -15,3 +15,17 @@ func ConvertToFloat64(inputs ...string) []float64 {
 
 	return result
 }
+
+// convert string to int
+func ConvertToInt64(inputs ...string) []int64 {
+	var result []int64
+	for _, value := range inputs {
+		parseValue, err := strconv.ParseInt(value, 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		result = append(result, parseValue)
+	}
+
+	return result
+}
