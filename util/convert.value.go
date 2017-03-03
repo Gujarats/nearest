20d5f4fed08ea4df09c97cfbee6dcc1e98d3ad69
gujarats <gujarat.santana@gmail.com>
2017-03-03 22:46:52 +0700

    return error when failed and fix test

--- util/convert.value.go
+++ util/convert.value.go
@@ -3,29 +3,29 @@ package util
 import "strconv"
 
 // Convert string to float64.
-func ConvertToFloat64(inputs ...string) []float64 {
+func ConvertToFloat64(inputs ...string) ([]float64, error) {
 	var result []float64
 	for _, value := range inputs {
 		parseValue, err := strconv.ParseFloat(value, 64)
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 		result = append(result, parseValue)
 	}
 
-	return result
+	return result, nil
 }
 
 // convert string to int
-func ConvertToInt64(inputs ...string) []int64 {
+func ConvertToInt64(inputs ...string) ([]int64, error) {
 	var result []int64
 	for _, value := range inputs {
 		parseValue, err := strconv.ParseInt(value, 10, 64)
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 		result = append(result, parseValue)
 	}
 
-	return result
+	return result, nil
 }
--- util/convert.value_test.go
+++ util/convert.value_test.go
@@ -19,8 +19,34 @@ func TestConverToFloat64(t *testing.T) {
 	}
 
 	for _, test := range testObjects {
-		result := ConvertToFloat64(test.Input1, test.Input2, test.Input3, test.Input3)
+		result, err := ConvertToFloat64(test.Input1, test.Input2, test.Input3, test.Input3)
+		if err != nil {
+			t.Errorf("Error expected no error happen from the inputs given")
+		}
 		fmt.Printf(" result = %+v\n", result)
 	}
 
+}
+
+func TesstConvertToInt(t *testing.T) {
+
+	type testObject struct {
+		Input1 string
+		Input2 string
+		Input3 string
+		Input4 string
+	}
+
+	testObjects := []testObject{
+		{Input1: "1.2", Input2: "2.43434", Input3: "5.23232", Input4: "8.3423432"},
+		{Input1: "2.2", Input2: "23.43434", Input3: "15.23232", Input4: "18.3423432"},
+	}
+
+	for _, test := range testObjects {
+		result, err := ConvertToInt64(test.Input1, test.Input2, test.Input3, test.Input3)
+		if err != nil {
+			t.Errorf("Error expected no error happen from the inputs given")
+		}
+		fmt.Printf(" result = %+v\n", result)
+	}
 }
