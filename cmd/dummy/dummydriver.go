ebf3fdc4bf2eee5cf3d63e4ff1bf46c8ecc921b7
gujarats <gujarat.santana@gmail.com>
2017-03-13 10:30:20 +0700

    create dummy data to redis

--- cmd/dummy/dummydriver.go
+++ cmd/dummy/dummydriver.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
 	"github.com/Gujarats/API-Golang/database"
 	"github.com/Gujarats/GenerateLocation"
 	"github.com/icrowley/fake"
 	mgo "gopkg.in/mgo.v2"
+	redis "gopkg.in/redis.v5"
 
 	driverModel "github.com/Gujarats/API-Golang/model/driver"
 )
@@ -15,13 +22,21 @@ func main() {
 
 	// get mongodb connection
 	mongoConn := listConnection["mongodb"].(*mgo.Session)
+	redisConn := listConnection["redis"].(*redis.Client)
 
 	// init driver model
 	driverData := &driverModel.DriverData{}
 	driverData.GetConn(mongoConn)
 
 	// inserting dummy driver
-	insertDummyDriver(driverData)
+	//insertDummyDriver(driverData)
+
+	//seed redis
+	startTime := time.Now()
+	seedDriverRedis(redisConn)
+	GetDriverRedis(redisConn)
+	fmt.Printf("takes time = %.6f\n", time.Since(startTime).Seconds())
+
 }
 
 // insert database 50.000 rows
@@ -35,11 +50,54 @@ func insertDummyDriver(driverData *driverModel.DriverData) {
 
 }
 
+func seedDriverRedis(redisConn *redis.Client) {
+	// create 20 driver
+	drivers := GenereateDriver(20)
+	city := City{
+		Name: "Paris",
+		Lat:  48.8588377,
+		Lon:  2.2775176,
+	}
+
+	byteDrivers, _ := json.Marshal(drivers)
+	byteCity, _ := json.Marshal(city)
+	data := string(byteCity) + "++" + string(byteDrivers)
+
+	redisConn.Set("someKey", data, 0)
+}
+
+func GetDriverRedis(redisConn *redis.Client) {
+	var city City
+	var drivers []Driver
+
+	dataString, err := redisConn.Get("someKey").Result()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// split data with ++
+	dataSplit := strings.Split(dataString, "++")
+	byteCity := []byte(dataSplit[0])
+	byteDrivers := []byte(dataSplit[1])
+
+	err = json.Unmarshal(byteCity, &city)
+	err = json.Unmarshal(byteDrivers, &drivers)
+
+	//fmt.Printf("city = %+v\n", city)
+	//fmt.Printf("drivers = %+v\n", drivers)
+}
+
 type Driver struct {
 	Name   string  `json:"name"`
 	Lat    float64 `json:"lat"`
 	Lon    float64 `json:"lon"`
 	Status bool    `json:"status"`
+}
+
+type City struct {
+	Name string  `json:"name"`
+	Lat  float64 `json:"lat"`
+	Lon  float64 `json:"lon"`
 }
 
 func GenereateDriver(sum int) []Driver {
