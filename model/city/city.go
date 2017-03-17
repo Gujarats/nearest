759ef6b417282f6a33db9b200edd8aee74eaa1f3
gujarats <gujarat.santana@gmail.com>
2017-03-17 21:38:49 +0700

    create index for driver

---
+++ model/city/city.go
@@ -0,0 +1,107 @@
+package city
+
+import (
+	"errors"
+
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	redis "gopkg.in/redis.v5"
+)
+
+type City struct {
+	Id       bson.ObjectId `json:"id" bson:"_id,omitempty"`
+	Name     string        `json:"name"`
+	District int           `json:"district"`
+	Location GeoJson       `json:"location"`
+}
+
+type GeoJson struct {
+	Type        string    `json:"type"`
+	Coordinates []float64 `json:"coordinates"`
+}
+
+// Global variable for storing database connection.
+var mongo *mgo.Session
+var redisConn *redis.Client
+
+func (c *City) GetConn(mongoConnection *mgo.Session, redisConnection *redis.Client) {
+	mongo = mongoConnection
+	redisConn = redisConnection
+}
+
+func checkMongoConnection(mongoConnection *mgo.Session) error {
+	if mongoConnection == nil {
+		return errors.New("No Mongo Connection")
+	}
+
+	return nil
+}
+
+// Inserting district to mongo database
+func (c *City) InsertDistrict(city string, distric int, lat, lon float64) error {
+	var err error
+	err = checkMongoConnection(mongo)
+	if err != nil {
+		return err
+	}
+
+	collection := mongo.DB("Driver").C(city)
+
+	c.District = distric
+	c.Name = city
+	c.Location = GeoJson{Type: "point", Coordinates: []float64{lon, lat}} // lon, lat order rules from mongodb
+
+	err = collection.Insert(c)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *City) AllDistrict(city string) ([]City, error) {
+	var cities []City
+	var err error
+
+	err = checkMongoConnection(mongo)
+	if err != nil {
+		return cities, err
+	}
+
+	collection := mongo.DB("Driver").C(city)
+	err = collection.Find(bson.M{}).All(&cities)
+	if err != nil {
+		return cities, err
+	}
+
+	return cities, nil
+}
+
+func (c *City) GetNearestDistrict(cityName string, lat, lon float64, distance int64) (City, error) {
+	var err error
+	var city City
+
+	err = checkMongoConnection(mongo)
+	if err != nil {
+		return city, err
+	}
+
+	collection := mongo.DB("Driver").C(cityName)
+
+	err = collection.Find(bson.M{
+		"location": bson.M{
+			"$nearSphere": bson.M{
+				"$geometry": bson.M{
+					"type":        "Point",
+					"coordinates": []float64{lon, lat}, // lon,lat in order is the rule from mongodb
+				},
+				"$maxDistance": distance,
+			},
+		},
+	}).One(&city)
+	if err != nil {
+		return city, err
+	}
+
+	return city, nil
+}
--- model/driver/driver.go
+++ model/driver/driver.go
@@ -3,10 +3,10 @@ package driver
 import (
 	"encoding/json"
 	"log"
+	"os"
 	"strings"
 	"time"
 
-	"github.com/Gujarats/API-Golang/util/logger"
 	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 	redis "gopkg.in/redis.v5"
@@ -34,14 +34,17 @@ type City struct {
 // Global variable for storing database connection.
 var mongo *mgo.Session
 var redisConn *redis.Client
+var logger *log.Logger
+
+func init() {
+	logger = log.New(os.Stderr,
+		"Driver",
+		log.Ldate|log.Ltime|log.Lshortfile)
+}
 
 func (d *DriverData) GetConn(mongoSession *mgo.Session, redisConnection *redis.Client) {
 	mongo = mongoSession
 	redisConn = redisConnection
-}
-
-func init() {
-	logger.InitLogger("Model Driver", "../../logs/", "Model.txt")
 }
 
 //===================REDIS====================//
@@ -56,13 +59,14 @@ func (d *DriverData) SaveDriversRedis(drivers []DriverData, city City) {
 }
 
 // the format of the key is : city;date
+// return the Drivers data from redis
 func (d *DriverData) DriversRedis(key string) (City, []DriverData) {
 	var city City
 	var drivers []DriverData
 
 	dataString, _ := redisConn.Get(key).Result()
 	if dataString == "" {
-		log.Println("Drivers in redis nil")
+		logger.Println("Drivers in redis nil")
 		return city, drivers
 	}
 
@@ -73,11 +77,11 @@ func (d *DriverData) DriversRedis(key string) (City, []DriverData) {
 
 	err := json.Unmarshal(byteCity, &city)
 	if err != nil {
-		log.Println(err)
+		logger.Println(err)
 	}
 	err = json.Unmarshal(byteDrivers, &drivers)
 	if err != nil {
-		log.Println(err)
+		logger.Println(err)
 	}
 
 	return city, drivers
@@ -103,13 +107,13 @@ func (d *DriverData) GetNearLocation(distance int64, lat, lon float64) []DriverD
 	}).Limit(100).All(&driverLocation)
 
 	if err != nil {
-		logger.CheckError("Mongo", err)
+		logger.Println(err)
 	}
 
 	return driverLocation
 }
 
-func (d *DriverData) Insert(name string, lat, lon float64, status bool) {
+func (d *DriverData) Insert(collectionName string, name string, lat, lon float64, status bool) {
 	// init value data
 	d.Name = name
 	d.Status = status
@@ -117,16 +121,34 @@ func (d *DriverData) Insert(name string, lat, lon float64, status bool) {
 
 	// set the slice to empty,
 	// to make sure there are no additional or old data exist in the slice.
-	d.Location.Coordinates = []float64{}
-	d.Location.Coordinates = append(d.Location.Coordinates, lon)
-	d.Location.Coordinates = append(d.Location.Coordinates, lat)
+	d.Location.Coordinates = []float64{lon, lat}
 
-	collection := mongo.DB("Driver").C("driver")
+	collection := mongo.DB("Driver").C(collectionName)
 
 	err := collection.Insert(d)
+	if err != nil {
+		logger.Println(err)
+	}
 
-	logger.CheckError("Model Driver", err)
+}
 
+// create index for location and status for speed read query.
+func (d *DriverData) CreateIndex(collectionName string) error {
+
+	collection := mongo.DB("Driver").C(collectionName)
+	index := mgo.Index{
+		Key: []string{
+			"$2dsphere:location",
+			"status",
+		},
+	}
+	err := collection.EnsureIndex(index)
+	if err != nil {
+
+		return err
+	}
+
+	return nil
 }
 
 func (d *DriverData) Find(name string) *DriverData {
@@ -147,6 +169,6 @@ func (d *DriverData) Update(driver DriverData) {
 
 	_, err := collection.Upsert(bson.M{"_id": driver.Id}, driver)
 
-	logger.CheckError("Model Driver", err)
+	logger.Println(err)
 
 }
