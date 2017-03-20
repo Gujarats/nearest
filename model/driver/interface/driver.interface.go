0828485a3165c06a96da701b9a3bbdff20dcabcd
gujarats <gujarat.santana@gmail.com>
2017-03-20 14:14:20 +0700

    fix build for app.go

--- model/driver/interface/driver.interface.go
+++ model/driver/interface/driver.interface.go
@@ -8,7 +8,7 @@ import (
 
 type DriverInterfacce interface {
 	GetConn(mongoSession *mgo.Session, redsiConn *redis.Client)
-	Insert(name string, lat float64, lon float64, status bool)
+	Insert(collectionName string, name string, lat, lon float64, status bool)
 	Find(name string) *driver.DriverData
 	Update(driverData driver.DriverData)
 	GetNearLocation(distance int64, lat, lon float64) []driver.DriverData
