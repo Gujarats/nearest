4e00cb98ac8de721aaa387cd23576e24f194a602
gujarats <gujarat.santana@gmail.com>
2017-03-20 21:28:20 +0700

    remove unnecessary files

--- model/review/interface/review.interface.go
+++
@@ -1,8 +0,0 @@
-package reviewInterface
-
-import "github.com/jmoiron/sqlx"
-
-type ReviewInterface interface {
-	GetConn(*sqlx.DB)
-	Exist() bool
-}
--- model/shop/type.activeSeller.go
+++
@@ -1,9 +0,0 @@
-package shop
-
-// this file use defining Active seller and its core behaviour
-
-type ActiveSeller struct {
-	Date              string
-	ShopId            int64
-	TotalActiveSeller string
-}
