b88b07523a2f121be1404b17981fa4bc7499f8aa
gujarats <gujarat.santana@gmail.com>
2017-03-25 18:59:24 +0700

    fix eof on file when reading after logger use it

--- util/logger/logger.file_test.go
+++ util/logger/logger.file_test.go
@@ -8,33 +8,36 @@ import (
 )
 
 var file *os.File
+var logger *log.Logger
+var filePath string
+var fileName string
 
 func init() {
+	filePath = "./testLog/"
+	fileName = "hello.txt"
 	var err error
-	file, err = createLogFile("./testLog/", "hello.txt")
+	file, err = createLogFile(filePath, fileName)
 	if err != nil {
 		panic(err)
 	}
+
+	logger = log.New(os.Stderr,
+		"Test Log :: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 // checking if file created is exist
 func TestCreateLogFile(t *testing.T) {
 	//checking file location
-	_, err := os.Stat("./testLog/hello.txt")
+	_, err := os.Stat(filePath + fileName)
 	if err != nil {
 		t.Errorf("Expecting the file exist got = %v\n", err)
 	}
 }
 
 func TestWriteLog(t *testing.T) {
-	filePath := "./testLog/"
-	fileName := "hello.txt"
+	// set file and print some error to the error message to file.
 	inputData := "data log here"
-
-	// set input error
-	logger := log.New(os.Stderr,
-		"Test Log :: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
 	logger.SetOutput(file)
 	logger.Println(inputData)
 
@@ -44,15 +47,15 @@ func TestWriteLog(t *testing.T) {
 		t.Errorf("Error file is not exist = %v\n", err)
 	}
 
-	//opening file
-	f, err := os.OpenFile(filePath+fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
+	// opening file again since we used a pointer of file above on logger.
+	file, err = os.OpenFile(filePath+fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
 	if err != nil {
 		t.Errorf("Error opening file = %v\n", err)
 	}
 
 	//get the data from the file
 	result := make([]byte, 100)
-	_, err = f.Read(result)
+	_, err = file.Read(result)
 	if err != nil {
 		t.Errorf("Cannot read file = %v\n", err)
 	}
