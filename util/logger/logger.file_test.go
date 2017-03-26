3e47ae344c53b67c11b4e08831262f02b7b81209
gujarats <gujarat.santana@gmail.com>
2017-03-26 11:26:04 +0700

    remove some test case on file

--- util/logger/logger.file_test.go
+++ util/logger/logger.file_test.go
@@ -1,13 +1,11 @@
 package logger
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"testing"
 )
 
-var file *os.File
 var logger *log.Logger
 var filePath string
 var fileName string
@@ -15,12 +13,6 @@ var fileName string
 func init() {
 	filePath = "./testLog/"
 	fileName = "hello.txt"
-	var err error
-	file, err = createLogFile(filePath, fileName)
-	if err != nil {
-		panic(err)
-	}
-
 	logger = log.New(os.Stderr,
 		"Test Log :: ",
 		log.Ldate|log.Ltime|log.Lshortfile)
@@ -38,33 +30,13 @@ func TestCreateLogFile(t *testing.T) {
 func TestWriteLog(t *testing.T) {
 	// set file and print some error to the error message to file.
 	inputData := "data log here"
+
+	file, err := createLogFile(filePath, fileName)
+	if err != nil {
+		t.Error(err)
+	}
+	defer file.Close()
+
 	logger.SetOutput(file)
 	logger.Println(inputData)
-
-	//checking file location
-	_, err := os.Stat(filePath + fileName)
-	if err != nil {
-		t.Errorf("Error file is not exist = %v\n", err)
-	}
-
-	// opening file again since we used a pointer of file above on logger.
-	file, err = os.OpenFile(filePath+fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
-	if err != nil {
-		t.Errorf("Error opening file = %v\n", err)
-	}
-
-	//get the data from the file
-	result := make([]byte, 100)
-	_, err = file.Read(result)
-	if err != nil {
-		t.Errorf("Cannot read file = %v\n", err)
-	}
-
-	if len(result) == 0 {
-		t.Error("Log Data is Empty")
-	}
-
-	//convert byte to string
-	stringResult := string(result)
-	fmt.Println("Result Read files = ", stringResult)
 }
